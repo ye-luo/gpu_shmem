68faf6c243ed637c95a094d57a0035f30067ee12
Ye Luo <yeluo@anl.gov>
2026-03-01 13:43:27 -0600

    Prevent ishmem initializing mpi.

--- ishmem/app.cpp
+++ ishmem/app.cpp
@@ -7,10 +7,8 @@
 #define N 10
 
 int main() {
-  // MPI_Init_thread MPI_THREAD_MULTIPLE seems required. Otherwise, got
-  // [0000] WARN: ishmemi_runtime_mpi
-  // [0000]        Failed to initialize MPI with MPI_THREAD_MULTIPLE
   int provided;
+  // FUNNELED or MPI_Init hit segfault.
   MPI_Init_thread(NULL, NULL, MPI_THREAD_MULTIPLE, &provided);
   MPI_Comm mpi_comm = MPI_COMM_WORLD;
   ishmemx_attr_t attr;
@@ -18,6 +16,7 @@ int main() {
   /* Initialize NVSHMEM */
   attr.mpi_comm = &mpi_comm;
   attr.runtime = ISHMEMX_RUNTIME_MPI;
+  attr.initialize_runtime = false;
   ishmemx_init_attr(&attr);
 
   int my_pe = ishmem_my_pe();
